d45f32177b625dc9e62cb509dcb637fd9de74d2b
Eva Coffield <eva@darcoff.com>
2020-10-05 22:05:32 +0100

    Created Clock component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Clock from './components/Clock';
 import './App.css';
+import { Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2020',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    //this.setState({deadline: 'November 25, 2020'})
+    //console.log('state', this.state);
+    this.setState({deadline: this.state.newDeadline})
+  }
+
+  state = {  }
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col>
+              <div className="App-title">
+                Countdown to {this.state.deadline}
+              </div>
+              <Clock 
+              deadline={this.state.deadline}
+              />
+              <Form inline>
+                <FormControl
+                  className="Deadline-input"
+                  placeholder="new date"
+                  //onChange={event => console.log('event', event.target.value)}
+                  onChange={event => this.setState({newDeadline: event.target.value})}
+                  />
+                <Button onClick={()=> this.changeDeadline()}>Submit</Button>
+              </Form>
+            </Col>
+          </Row>
+         </Container>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
